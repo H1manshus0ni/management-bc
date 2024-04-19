a2dc39768d613d06bb36e19fa763eea481578d2a
Himanshu Soni <himanshuso912@gmail.com>
2024-04-19 17:30:20 +0530

    Initialised

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL)
+        console.log("Connection Establised")
+    }catch(err) {
+        console.log("Connection Failed with DB",err)
+    }
+}
+
+// db.on("error",(err)=>console.log(err));
+// db.on("open",()=>console.log("DATABASE CONNECTED"));
+
+const tasRouter = require("./routes/tasks");
+app.use("/api/tasks",tasRouter)
+
+
+
+
+
+app.listen(process.env.PORT,()=> {
+    connectDB();
+    console.log(`server is listening at port ${process.env.PORT}`)
+}
+);
